fcd69ef2fe4c63c602080b74222e2717a048d176
MuizMK <jelajahit2014@gmail.com>
2024-03-01 22:57:31 +0900

    r5

--- page2.py
+++ page2.py
@@ -4,9 +4,9 @@ def page_2():
     st.title('Page 2')
     st.write('Ini adalah konten untuk Page 2')
     # st.video("https://www.youtube.com/watch?v=2qcsb8n55Ds")
-    st.video("https://www.youtube.com/watch?v=2qcsb8n55Ds", format='video/mp4', start_time=0)
+    st.video("https://www.youtube.com/watch?v=PrwSPTDqf_o")
 
-    st.title('Rumus Umum Turunan Fungsi')
+    st.write('Rumus Umum Turunan Fungsi')    
     st.latex(r'''
     f'(x) = \mathop {\lim }\limits_{h \to 0} \,\,\frac{{f(x + h) - f(x)}}{h}
     ''')
