c2987fb7b4afe9f504fe6cdeace7c6aa0b51aa14
MuizMK <jelajahit2014@gmail.com>
2024-03-01 22:42:25 +0900

    r4

--- main.py
+++ main.py
@@ -4,16 +4,6 @@ from page2 import page_2  # Import fungsi page_2 dari file page2.py
 from page3 import page_3  # Import fungsi page_2 dari file page2.py
 from page4 import page_4  # Import fungsi page_2 dari file page2.py
 
-# Definisikan fungsi untuk halaman pertama (page_1) dan halaman ketiga (page_3) di sini
-
-# Fungsi untuk halaman pertama
-# def page_1():
-#     st.title('Page 1')
-#     st.write('Deskripsi aplikasi Anda di sini')
-
-
-# Fungsi untuk halaman ketiga
-
 PAGES = {
     "Page 1": page_1,
     "Page 2": page_2,
@@ -24,9 +14,6 @@ PAGES = {
 # Sidebar
 st.sidebar.image("logo.png", width=100)  # Ganti "logo.png" dengan path file logo Anda
 page = st.sidebar.radio("Pilih Halaman", list(PAGES.keys()))
-# st.sidebar.checkbox("Checkbox 01")
-# st.sidebar.checkbox("Checkbox 02")
-# st.sidebar.selectbox("ComboBox", ["Option1", "Option2", "Option3"])
 
 # Konten Utama
 PAGES[page]()
--- page1.py
+++ page1.py
@@ -3,3 +3,8 @@ import streamlit as st
 def page_1():
     st.title('Page 1')
     st.write('Ini adalah konten untuk Page 1')
+    with open("halo.md", "r") as file:
+        markdown_text = file.read()
+
+    # Tampilkan konten Markdown di aplikasi Streamlit
+    st.markdown(markdown_text)
